cd916f68986c7de1646a80dd1d9185052d157107
Parshant17 <136882211+Parshant17@users.noreply.github.com>
2025-06-09 10:13:41 +0530

    Time: 1 ms (99.56%) | Memory: 45 MB (97.50%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validMountainArray(int[] A) {
+        int len =A.length;
+        if(len ==0){
+            return false;
+        }
+        int i=0;
+        for( i=0 ;i+1<len ;i++){
+            if(A[i]<A[i+1]){
+
+            }else{
+                break;
+            }
+        }
+        if(i==0 || i==len-1){
+            return false;
+        }
+        for( ;i+1<len ;i++){
+            if(A[i]>A[i+1]){
+
+            }else{
+                break;
+            }
+        }
+        return i ==(len-1);
+    }
+}
